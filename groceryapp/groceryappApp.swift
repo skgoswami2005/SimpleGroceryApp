db2ada5d7dfefae93cdced0080b06f9bddbe1d0f
skgoswami2005 <skgoswamimailbox@gmail.com>
2024-03-13 22:32:42 +0530

    Initial Commit

---
+++ groceryapp/groceryappApp.swift
@@ -0,0 +1,17 @@
+//
+//  groceryappApp.swift
+//  groceryapp
+//
+//  Created by Shubham on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct groceryappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
