406f4938828a5e032c0bafd61b390155fa959c4a
skgoswami2005 <skgoswamimailbox@gmail.com>
2024-03-13 23:02:26 +0530

    created first page

---
+++ groceryapp/Components/ShopItem.swift
@@ -0,0 +1,18 @@
+//
+//  ShopItem.swift
+//  groceryapp
+//
+//  Created by Shubham on 13/03/24.
+//
+
+import SwiftUI
+
+struct ShopItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShopItem()
+}
---
+++ groceryapp/Views/Shop.swift
@@ -0,0 +1,18 @@
+//
+//  Shop.swift
+//  groceryapp
+//
+//  Created by Shubham on 13/03/24.
+//
+
+import SwiftUI
+
+struct Shop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Shop()
+}
